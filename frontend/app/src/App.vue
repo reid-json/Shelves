3a33d9a644d288fe92298085e9405af026c1772c
jason <jasonreidjr1@gmail.com>
2025-10-24 18:14:06 -0500

    frontend animation

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,30 +1,113 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="vinyl-container">
+    <img
+        src="/images/vinyl.png"
+        class="vinyl"
+        :class="{ spin: isPlaying }"
+        @click="playAlbum"
+    />
+    <img
+        src="/images/HOB.jpg"
+        class="sleeve"
+        :class="{ open: isOpen }"
+    />
+    <button @click="playAlbum">Play Album</button>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+export default {
+  data() {
+    return {
+      isOpen: false,
+      isPlaying: false,
+    };
+  },
+  methods: {
+    playAlbum() {
+      if (!this.isOpen) {
+        this.isOpen = true;
+        setTimeout(() => {
+          this.isPlaying = true;
+          const audio = new Audio('/audio/HouseOfBallons/tildawn.mp3');
+          audio.play();
+        }, 1000);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+.vinyl-container {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  overflow: hidden;
+  margin: 50px auto;
+  background-color: #111;
+  border-radius: 10px;
+  padding: 10px;
 }
-.logo:hover {
-  filter: drop-shacd dow(0 0 2em #646cffaa);
+
+button {
+  position: absolute;
+  bottom: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 3;
+  background-color: #ff0000;
+  border: none;
+  padding: 8px 16px;
+  cursor: pointer;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.vinyl-container {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  overflow: hidden;
+  margin: 50px auto;
+  background-color: #111;
+  border-radius: 10px;
+  padding: 10px;
+}
+
+.vinyl {
+  position: absolute;
+  width: 100%;
+  height: auto;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  cursor: pointer;
+}
+
+.vinyl.spin {
+  animation: spin 3s linear infinite;
+}
+
+.sleeve {
+  position: absolute;
+  width: 100%;
+  height: auto;
+  top: 0;
+  left: 0;
+  z-index: 2;
+  transition: transform 1s ease;
+}
+
+.sleeve.open {
+  transform: translateX(-120%);
+  pointer-events: none;
+}
+
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
 }
 </style>
